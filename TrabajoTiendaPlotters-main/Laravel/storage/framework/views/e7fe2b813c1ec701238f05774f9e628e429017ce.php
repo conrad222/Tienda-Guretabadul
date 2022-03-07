c41b60457368d877914dc7593f7f5649241a1da6
conrad222 <conrado.ayuso@gmail.com>
2022-03-07 21:49:35 +0100

    asdf

--- TrabajoTiendaPlotters-main/Laravel/storage/framework/views/e7fe2b813c1ec701238f05774f9e628e429017ce.php
+++ TrabajoTiendaPlotters-main/Laravel/storage/framework/views/e7fe2b813c1ec701238f05774f9e628e429017ce.php
@@ -1,23 +1,21 @@
 
     
-      <footer class="footer text-center">
+      <footer class="footer text-center" style="background: repeating-linear-gradient(45deg, #4e9b7a, #6f8ba7 100px); font-weight: bold; font-family: cursive;">
         <div class="container">
             <div class="row">
                 <!-- Footer Location-->
                 
                 <!-- Footer Social Icons-->
-                <div class="col-lg-4 mb-5 mb-lg-0">
+                <div class="mb-lg-0" style="justify-content:align-center; padding-top: 12px; ">
+                    <a  href="https://www.instagram.com/guretabadul/" style="text-decoration:none; ">instagram<i class="fab fa-fw fa-instagram fa-2x" id="instagram" style="margin-right:50px;"></i></a>
+                    <a  href="https://www.youtube.com/channel/UCiFnpQL4o1KoVfdjr0PM_7w" style="text-decoration:none;">youtube<i class="fab fa-fw fa-youtube fa-2x" id="youtube" style="margin-right:50px;"></i></a>
+                    <a  href="https://www.instagram.com/guretabadul/" style="text-decoration:none;">facebook<i class="fab fa-fw fa-facebook fa-2x" id="facebook" style="margin-right:50px;"></i></a>
+                    <a  href="https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin" style="text-decoration:none;">kaixo@guretabadul.eu<i class="fa-solid fa-at fa-2x" id="gmail" style="margin-right:50px;"></i></a>
+                    <a  href="https://www.google.es/maps/place/Calle+del+Dos+de+Mayo,+48003+Bilbao,+Vizcaya/@43.2573926,-2.9308172,17z/data=!3m1!4b1!4m5!3m4!1s0xd4e4fd23e2b219d:0x69431385f0a72f5c!8m2!3d43.2573887!4d-2.9286285" style="text-decoration:none;">C/ Dos de Mayo, Bilbao<i class="fa-solid fa-location-dot fa-2x" style="margin-right:50px;"></i></i></a>
                    
-                    
-                    <a  href="https://www.instagram.com/guretabadul/"><i class="fab fa-fw fa-instagram fa-2x" id="instagram"></i></a>
+                  
                 </div>
-                <!-- Footer About Text-->
-                <div class="col-lg-4">
-                    <p id="correo" class="lead mb-0">
-                        GURE TABADUL
-                        
-                    </p>
-                </div>
+                
                 
             </div>
         </div>
