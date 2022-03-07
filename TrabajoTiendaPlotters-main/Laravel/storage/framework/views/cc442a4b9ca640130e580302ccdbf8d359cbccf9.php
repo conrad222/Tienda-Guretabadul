d427c7468a5a6328276727aa895395c669aed3cc
IkerSimon <76107560+IkerSimon@users.noreply.github.com>
2022-03-07 14:29:30 +0100

    l

--- TrabajoTiendaPlotters-main/Laravel/storage/framework/views/cc442a4b9ca640130e580302ccdbf8d359cbccf9.php
+++ TrabajoTiendaPlotters-main/Laravel/storage/framework/views/cc442a4b9ca640130e580302ccdbf8d359cbccf9.php
@@ -1,11 +1,72 @@
 <style>
-    .nav-link:hover{
+
+.nav-link:hover{
         background-color: #B81F57;
         color: white!important;
-        border-radius: 20%;
+        border-radius: 100%;
     }
-    .nav-link{
-        transition: background-color 1s;
-        border-radius:20%;
-    }
+
+   .nav-link {
+    transition: background-color 1s;
+    border-radius:100%;
+    display: block;
+    padding: 0.5rem 1rem;
+    color: #64a19d;
+    text-decoration: none;
+    -webkit-animation: colorchange 20s infinite alternate;
+}
+
+.nav-link:hover, .nav-link:focus {
+  color: #50817e;
+}
+
+.nav-link.disabled {
+  color: #6c757d;
+  pointer-events: none;
+  cursor: default;
+}
+
+@-webkit-keyframes colorchange {
+  0% {
+    color: grey;
+  }
+  10% {
+    color: #8e44ad;
+  }
+  20% {
+    color: #1abc9c;
+  }
+  30% {
+    color: #d35400;
+  }
+  40% {
+    color: #924444;
+  }
+  50% {
+    color: crimson;
+  }
+  60% {
+    color: blue;
+  }
+  70% {
+    color: #2980b9;
+  }
+  80% {
+    color: #f1c40f;
+  }
+  90% {
+    color: chartreuse;
+  }
+  100% {
+    color: pink;
+  }
+}
+
+@media (prefers-reduced-motion: reduce) {
+  .nav-link {
+    -webkit-transition: none;
+    transition: none;
+  }
+}
+
 </style><?php /**PATH C:\Users\Iker\Desktop\UniServerZ\www\Tienda-Guretabadul\TrabajoTiendaPlotters-main\Laravel\resources\views/layout/estilos.blade.php ENDPATH**/ ?>
