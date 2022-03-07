91308e601cbbcee79b7f975645d7dc9d2a1329eb
Llegó Papá <76107506+feersuarez@users.noreply.github.com>
2022-03-07 12:04:50 +0100

    a

---
+++ TrabajoTiendaPlotters-main/Laravel/storage/framework/views/dd10290f5249424a8b6ecb2e7fae93c336ae8601.php
@@ -0,0 +1,8 @@
+<?php $__env->startSection('Titulo', 'Tienda'); ?>
+<?php $__env->startSection('contenido'); ?>
+<?php $__env->startSection('estilos'); ?>
+<?php $__env->stopSection(); ?>
+<p>Holaaaa</p>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout.masterpage', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\UniServerZ\www\Tienda-Guretabadul\TrabajoTiendaPlotters-main\Laravel\resources\views/secciones/tienda.blade.php ENDPATH**/ ?>
---
+++ TrabajoTiendaPlotters-main/Laravel/storage/framework/views/e7fe2b813c1ec701238f05774f9e628e429017ce.php
@@ -0,0 +1,24 @@
+
+    
+      <footer class="footer text-center">
+        <div class="container">
+            <div class="row">
+                <!-- Footer Location-->
+                
+                <!-- Footer Social Icons-->
+                <div class="col-lg-4 mb-5 mb-lg-0">
+                   
+                    
+                    <a  href="https://www.instagram.com/guretabadul/"><i class="fab fa-fw fa-instagram fa-2x" id="instagram"></i></a>
+                </div>
+                <!-- Footer About Text-->
+                <div class="col-lg-4">
+                    <p id="correo" class="lead mb-0">
+                        GURE TABADUL
+                        
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+    </footer><?php /**PATH D:\UniServerZ\www\Tienda-Guretabadul\TrabajoTiendaPlotters-main\Laravel\resources\views/layout/footer.blade.php ENDPATH**/ ?>
