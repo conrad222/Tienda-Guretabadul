ed16d12c37d8ad734219179b8fd01fbad48a33cd
feersuarez <76107506+feersuarez@users.noreply.github.com>
2022-03-09 16:52:32 +0100

    Si no va revertir

---
+++ TrabajoTiendaPlotters-main/Laravel/storage/framework/views/e79a4027dd638e4104faea361899f1b53aba7dd4.php
@@ -0,0 +1,138 @@
+<style>
+
+.nav-link:hover{
+        background-color: #B81F57;
+        color: white!important;
+        border-radius: 100%;
+    }
+
+   .nav-link {
+    transition: background-color 1s;
+    border-radius:100%;
+    display: block;
+    padding: 0.5rem 1rem;
+    color: #64a19d;
+    text-decoration: none;
+    -webkit-animation: colorchange 20s infinite alternate;
+}
+
+.nav-link:hover, .nav-link:focus {
+  color: #50817e;
+}
+
+.nav-link.disabled {
+  color: #6c757d;
+  pointer-events: none;
+  cursor: default;
+}
+
+@-webkit-keyframes colorchange {
+  0% {
+    color: grey;
+  }
+  10% {
+    color: #8e44ad;
+  }
+  20% {
+    color: #1abc9c;
+  }
+  30% {
+    color: #d35400;
+  }
+  40% {
+    color: #924444;
+  }
+  50% {
+    color: crimson;
+  }
+  60% {
+    color: blue;
+  }
+  70% {
+    color: #2980b9;
+  }
+  80% {
+    color: #f1c40f;
+  }
+  90% {
+    color: chartreuse;
+  }
+  100% {
+    color: pink;
+  }
+}
+
+@media (prefers-reduced-motion: reduce) {
+  .nav-link {
+    -webkit-transition: none;
+    transition: none;
+  }
+}
+
+.container-f{
+padding-right: 0.75rem;
+padding-left: 0.75rem;
+margin-right: auto;
+margin-left: auto;
+}
+
+.colores {
+    transition: background-color 1s;
+    border-radius:100%;
+    padding: 0.5rem 1rem;
+    color: #64a19d;
+    text-decoration: none;
+    -webkit-animation: colorchange 20s infinite alternate;
+}
+
+.colores.disabled {
+  color: #6c757d;
+  pointer-events: none;
+  cursor: default;
+}
+
+@-webkit-keyframes colorchange {
+  0% {
+    color: grey;
+  }
+  10% {
+    color: #8e44ad;
+  }
+  20% {
+    color: #1abc9c;
+  }
+  30% {
+    color: #d35400;
+  }
+  40% {
+    color: #924444;
+  }
+  50% {
+    color: crimson;
+  }
+  60% {
+    color: blue;
+  }
+  70% {
+    color: #2980b9;
+  }
+  80% {
+    color: #f1c40f;
+  }
+  90% {
+    color: chartreuse;
+  }
+  100% {
+    color: pink;
+  }
+}
+
+@media (prefers-reduced-motion: reduce) {
+  .colores{
+    -webkit-transition: none;
+    transition: none;
+  }
+}
+
+
+</style><?php /**PATH D:\UniServerZ\UniServerZ\www\Tienda-Guretabadul\TrabajoTiendaPlotters-main\Laravel\resources\views/layout/estilos.blade.php ENDPATH**/ ?>
