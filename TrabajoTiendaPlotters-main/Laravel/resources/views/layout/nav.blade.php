7712dbd1b55d4b0637bc1efae345fa85739bec70
feersuarez <76107506+feersuarez@users.noreply.github.com>
2022-03-09 17:56:45 +0100

    styles

--- TrabajoTiendaPlotters-main/Laravel/resources/views/layout/nav.blade.php
+++ TrabajoTiendaPlotters-main/Laravel/resources/views/layout/nav.blade.php
@@ -1,67 +1,65 @@
 @php
-    Illuminate\Support\Facades\App::setLocale(session('idioma'));
+Illuminate\Support\Facades\App::setLocale(session('idioma'));
 @endphp
 <script src="typed.js" type="text/javascript"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="https://unpkg.com/typeit@8.2.0/dist/index.umd.js"></script>
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background: repeating-linear-gradient(45deg, #4e9b7a, #6f8ba7 100px); font-weight: bold; font-family: cursive;
-">
-  <div class="container-fluid">
-    
-    <a id="tabadul" class="navbar-brand" href="home"></a>
-    <script>
-                    new TypeIt("#tabadul", {
-                      strings: "Gure Tabadul",
-                    }).go();
-                  </script>
- 
- 
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <div class="container-fluid">
 
+        <a id="tabadul" class="navbar-brand" href="home"></a>
+        <script>
+            new TypeIt("#tabadul", {
+                strings: "Gure Tabadul",
+            }).go();
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul style="margin-left:35%;" class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" aria-current="page" href="{{route('inicio.index')}}">Inicio</a>
-        </li>
+        </script>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul style="margin-left:35%;" class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" aria-current="page" href="{{route('inicio.index')}}">Inicio</a>
+                </li>
 
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="{{route('cursos.index')}}">Cursos</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="{{route('taller.index')}}">Taller</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="{{route('tienda.index')}}">Tienda</a>
-          </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{route('cursos.index')}}">Cursos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{route('taller.index')}}">Taller</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{route('tienda.index')}}">Tienda</a>
+                </li>
 
-        @if(!Auth::check())
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="{{route('login')}}">Inicio Sesión</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="{{route('register')}}">Registrarse</a>
-        </li>
-        @else
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="" onclick="event.preventDefault(); document.getElementById('logout').submit();">Cerrar sesion</a>
-          <!-- Solo usuarios identificados -->
-          <form id="logout" action="{{route('logout')}}" method="POST" style="display:none;">
-          @csrf
-          </form>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="{{route('perfil.index')}}">Ver perfil</a>
-        </li>
-        <a class="nav-link active" style="font-family:cursive; color:#000" >Bienvenida <b style="color: aquamarine">{{auth()->user()->name}}</b></a>
+                @if(!Auth::check())
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{route('login')}}">Inicio Sesión</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{route('register')}}">Registrarse</a>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href=""
+                        onclick="event.preventDefault(); document.getElementById('logout').submit();">Cerrar sesion</a>
+                    <!-- Solo usuarios identificados -->
+                    <form id="logout" action="{{route('logout')}}" method="POST" style="display:none;">
+                        @csrf
+                    </form>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{route('perfil.index')}}">Ver perfil</a>
+                </li>
+                <a class="nav-link active" style="font-family:cursive; color:#000">Bienvenida <b
+                        style="color: aquamarine">{{auth()->user()->name}}</b></a>
 
-        @endif
+                @endif
 
-      </ul>
+            </ul>
 
+        </div>
     </div>
-  </div>
 </nav>
-
--- TrabajoTiendaPlotters-main/Laravel/storage/framework/views/3f1adb234c4ad6938557b5567a7f9a10bd0ac1f7.php
+++ TrabajoTiendaPlotters-main/Laravel/storage/framework/views/3f1adb234c4ad6938557b5567a7f9a10bd0ac1f7.php
@@ -1,22 +1,28 @@
 
-    
-      <footer class="footer text-center" style="background: repeating-linear-gradient(45deg, #4e9b7a, #6f8ba7 100px); font-weight: bold; font-family: cursive;">
-        <div class="container">
-            <div class="row">
-                <!-- Footer Location-->
-                
-                <!-- Footer Social Icons-->
-                <div class="mb-lg-0" style="justify-content:align-center; padding-top: 12px; ">
-                    <a  href="https://www.instagram.com/guretabadul/" class="colores" style="text-decoration:none; ">instagram<i class="fab fa-fw fa-instagram fa-2x" id="instagram" style="margin-right:50px;"></i></a>
-                    <a  href="https://www.youtube.com/channel/UCiFnpQL4o1KoVfdjr0PM_7w"  class="colores" style="text-decoration:none;">youtube<i class="fab fa-fw fa-youtube fa-2x" id="youtube" style="margin-right:50px;"></i></a>
-                    <a  href="https://www.instagram.com/guretabadul/"  class="colores" style="text-decoration:none;">facebook<i class="fab fa-fw fa-facebook fa-2x" id="facebook" style="margin-right:50px;"></i></a>
-                    <a  href="https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin"  class="colores" style="text-decoration:none;">kaixo@guretabadul.eu<i class="fa-solid fa-at fa-2x" id="gmail" style="margin-right:50px;"></i></a>
-                    <a  href="https://www.google.es/maps/place/Calle+del+Dos+de+Mayo,+48003+Bilbao,+Vizcaya/@43.2573926,-2.9308172,17z/data=!3m1!4b1!4m5!3m4!1s0xd4e4fd23e2b219d:0x69431385f0a72f5c!8m2!3d43.2573887!4d-2.9286285" class="colores" style="text-decoration:none;">C/ Dos de Mayo, Bilbao<i class="fa-solid fa-location-dot fa-2x" style="margin-right:50px;"></i></i></a>
-                   
-                  
-                </div>
-                
-                
+<link rel="stylesheet" href="<?php echo e(URL::asset('scss/footer.css')); ?>">
+
+<footer class="footer text-center">
+    <div class="container">
+        <div class="row">
+            <!-- Footer Location-->
+
+            <!-- Footer Social Icons-->
+            <div class="mb-lg-0" style="justify-content:align-center; padding-top: 12px; ">
+                <a href="https://www.instagram.com/guretabadul/" style="text-decoration:none; ">instagram<i
+                        class="fab fa-fw fa-instagram fa-2x" id="instagram" style="margin-right:50px;"></i></a>
+                <a href="https://www.youtube.com/channel/UCiFnpQL4o1KoVfdjr0PM_7w"
+                    style="text-decoration:none;">youtube<i class="fab fa-fw fa-youtube fa-2x" id="youtube"
+                        style="margin-right:50px;"></i></a>
+                <a href="https://www.instagram.com/guretabadul/" style="text-decoration:none;">facebook<i
+                        class="fab fa-fw fa-facebook fa-2x" id="facebook" style="margin-right:50px;"></i></a>
+                <a href="https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+                    style="text-decoration:none;">kaixo@guretabadul.eu<i class="fa-solid fa-at fa-2x" id="gmail"
+                        style="margin-right:50px;"></i></a>
+                <a href="https://www.google.es/maps/place/Calle+del+Dos+de+Mayo,+48003+Bilbao,+Vizcaya/@43.2573926,-2.9308172,17z/data=!3m1!4b1!4m5!3m4!1s0xd4e4fd23e2b219d:0x69431385f0a72f5c!8m2!3d43.2573887!4d-2.9286285"
+                    style="text-decoration:none;">C/ Dos de Mayo, Bilbao<i class="fa-solid fa-location-dot fa-2x"
+                        style="margin-right:50px;"></i></i></a>
             </div>
         </div>
-    </footer><?php /**PATH D:\UniServerZ\UniServerZ\www\Tienda-Guretabadul\TrabajoTiendaPlotters-main\Laravel\resources\views/layout/footer.blade.php ENDPATH**/ ?>
+    </div>
+</footer>
+<?php /**PATH D:\UniServerZ\UniServerZ\www\Tienda-Guretabadul\TrabajoTiendaPlotters-main\Laravel\resources\views/layout/footer.blade.php ENDPATH**/ ?>
