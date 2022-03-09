f00b4a62e047956e64dd181e0d86cc955004e883
conrad222 <76107590+conrad222@users.noreply.github.com>
2022-03-09 12:39:27 +0100

    asf

--- TrabajoTiendaPlotters-main/Laravel/resources/views/secciones/tienda.blade.php
+++ TrabajoTiendaPlotters-main/Laravel/resources/views/secciones/tienda.blade.php
@@ -20,4 +20,19 @@
     </ul>
     
 </div>
+<div class="slider" >
+    <ul>
+        <li style="list-style:none"><img src="{{url('img/cami.png')}}" alt="hola" ></li>
+       
+        <li style="list-style:none"><img src="{{url('img/bolsa online.png')}}"></li>
+      
+    </ul>
+    <ul style="display:flex; ">
+        <li style="list-style:none; padding-right:370px;">Camiseta XXL</li>
+       
+        <li style="list-style:none;  padding-right:20px;" >Bolsa XXL</li>
+      
+    </ul>
+    
+</div>
 @endsection
--- TrabajoTiendaPlotters-main/Laravel/storage/framework/views/dd10290f5249424a8b6ecb2e7fae93c336ae8601.php
+++ TrabajoTiendaPlotters-main/Laravel/storage/framework/views/dd10290f5249424a8b6ecb2e7fae93c336ae8601.php
@@ -20,6 +20,21 @@
     </ul>
     
 </div>
+<div class="slider" >
+    <ul>
+        <li style="list-style:none"><img src="<?php echo e(url('img/cami.png')); ?>" alt="hola" ></li>
+       
+        <li style="list-style:none"><img src="<?php echo e(url('img/bolsa online.png')); ?>"></li>
+      
+    </ul>
+    <ul style="display:flex; ">
+        <li style="list-style:none; padding-right:370px;">Camiseta XXL</li>
+       
+        <li style="list-style:none;  padding-right:20px;" >Bolsa XXL</li>
+      
+    </ul>
+    
+</div>
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layout.masterpage', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\UniServerZ\www\Tienda-Guretabadul\TrabajoTiendaPlotters-main\Laravel\resources\views/secciones/tienda.blade.php ENDPATH**/ ?>
